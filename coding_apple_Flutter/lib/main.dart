2047cef7c8161039f91213dd45b650ad6e53b54b
woogie <jeonguk29@naver.com>
2022-06-28 16:58:27 +0900

    코딩애플 플러터 하단바 꾸미기 과제 수행

--- coding_apple_Flutter/lib/main.dart
+++ coding_apple_Flutter/lib/main.dart
@@ -10,9 +10,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home:Center(
-          child:  Container(width: 50, height: 50,color: Colors.blue),
-        )
-    );
+        home:Scaffold(
+        appBar: AppBar(
+          title: Text("앱제목"),
+        ),
+          body: Text("안녕"),
+          bottomNavigationBar:
+          BottomAppBar(
+            child: Container(
+              height : 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
+              ),
+            ),
+          ),
+/*
+    Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,    // row 일때 가로 정렬 Column 일때는 세로 정렬임
+    //crossAxisAlignment: CrossAxisAlignment.center,   세로축 정렬
+
+    children: const [
+    Icon(Icons.star),
+    Icon(Icons.star),
+    Icon(Icons.star),
+    Icon(Icons.star),
+    ],
+    */
+        ),
+
+        );
   }
 }
