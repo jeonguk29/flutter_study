67ce7020f8f7c00e0f9815a5e00bda4905e9e163
woogie <jeonguk29@naver.com>
2022-07-01 01:02:30 +0900

    커스텀위젯_ListView위젯 학습

--- coding_apple_Flutter/lib/main.dart
+++ coding_apple_Flutter/lib/main.dart
@@ -4,6 +4,10 @@ void main() {
   runApp(const MyApp()); // 앱 메인 페이지 입력하면 됩니다.
 }
 
+var a = SizedBox(
+  child: Text('안녕'),
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -23,46 +27,73 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-          body: Container(
-            height: 150,
-            padding: EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Image.asset("koko1.png", width: 150,),
-                Flexible(flex: 10,child:
-                Container(
+          body: // ShopItem(),  커스텀 위젯 사용법
+          ListView(
+            children: [
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
+              Text("안녕"),
 
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("카메라 팝니다."),
-                      Text("금호동 3가"),
-                      Text("7000원"),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(Icons.favorite),
-                          Text("4"),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                ),
-              ],
-            ),
+            ],
           ),
 
-
         ),
-            /*
-             Icon(Icons.star, color: Color(0xFFFA0000),),  아이콘 꾸미기
-
-            글자 꾸미기
-            text("안녕하세요",
-              style: TextStyle(color: Color(0xFF645454),
-                fontSize: 50, fontWeight: FontWeight.w700,
-            ),*/
     );
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  const ShopItem ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+    child: Text('안녕'),
+  );
   }
 }
