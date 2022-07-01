4f8833da635ae677e9a6c3c69cc853c8a0110cdb
woogie <jeonguk29@naver.com>
2022-07-01 21:58:23 +0900

    (사용자별 좋아요 추천 기능 구현)_변수이름,출력문 수정

--- coding_apple_Flutter/lib/main.dart
+++ coding_apple_Flutter/lib/main.dart
@@ -16,7 +16,7 @@ class _MyAppState extends State<MyApp> {
   var a =1;                                           //변수가 자동으로 state임 StatefulWidget 에서는
   var name = "연락처앱";
   var name2 = ["김영숙", "홍길동", "피자집"];  //이래야 김영숙 2로 바뀔때 바로바로 보여질거임
-  var name3 = [0,0,0];  //이래야 김영숙 2로 바뀔때 바로바로 보여질거임
+  var like = [0,0,0];  //이래야 김영숙 2로 바뀔때 바로바로 보여질거임
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -48,14 +48,14 @@ class _MyAppState extends State<MyApp> {
               itemCount: 3,  // 3번 반복
               itemBuilder: (context,i){ // i 반복시 1씩 증가
                 return ListTile(
-                    leading: Text("${name3[i].toString()}"),  
+                    leading: Text(like[i].toString()),
                     // 3번 반복이니 0,1,2 의 값을 각각 가지고 있는 개별로 생각 하면 이해쉬움  
                     // 0번 이름 출력 
                 title: Text(name2[i]), 
 
                   trailing: ElevatedButton( child: Text("좋아요"), onPressed: (){
                   setState((){  // 0번 좋아요 누르면 
-                    name3[i]++;  // 0번 값 증가해서 다시 위에서 출력 되는 거고  이게 1, 2 도 따로 구현되는거임 
+                    like[i]++;  // 0번 값 증가해서 다시 위에서 출력 되는 거고  이게 1, 2 도 따로 구현되는거임
                   });
                 },
                 )
