851557ea2bafc04bc3bfb06bb82ec4ee55e07f0e
woogie <jeonguk29@naver.com>
2022-07-05 17:01:18 +0900

    외부패키지 설치 셋팅 권한 요청 팝업 띄우기 팝업은 어떨 때 띄우는게 좋은가 방법1( 현제는 선호하지 않는 방법)

--- coding_apple_Flutter/lib/main.dart
+++ coding_apple_Flutter/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';  //  외부 패키지 가져옴
 
 void main() {
   runApp(MaterialApp(
-      // 보통 앱만들때 이렇게 마테리얼 앱을 밖으로 빼줌
+    // 보통 앱만들때 이렇게 마테리얼 앱을 밖으로 빼줌
       home: MyApp() // 이렇게 해도 레이아웃 똑같음 마테리얼 안에 MyApp 즉 Scaffold가 들어가는 것임
-      ));
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -15,21 +16,37 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  getPermission() async {       // 패키지 만든 사람이 정한 사용 법임
+    var status = await Permission.contacts.status; // 연락처 권한 줬는지 여부
+    // dart특징 오래걸리는 줄은 제껴두고 다음줄 실행하려고 함
+    // await 하지만 기다렸다 다음줄 실행하라고 하는 문법임 아래 값도 없이 if 실행되면 안되니까
+    // await  막쓰는게 아니라 쓸수있는 코드들이 따로 있음
+    if (status.isGranted) {
+      print('허락됨');   // 허락하면 이거 실행됨
+    } else if (status.isDenied) {
+      print('거절됨');                           // 허락 안하면 이거 실행됨
+      Permission.contacts.request();  // 허락해달라고 팝업띄우는 코드
+      // openAppSettings(); 위에 팝업 2번 취소하면 다신 뜨지 않음 앱 정책임
+      // 그래서 주석한것처럼 직접 설정가서 켜달라고 하는게 좋음
+    }
+  }
+
+
+  void initState(){  // 유용한 함수인데 이 위젯 즉 Myapp 위젯이 처음 로드될때
+    super.initState();  // 이안에 코드를 한번 실행함
+    getPermission(); // 우리가 방금 허락 요청하는 함수도 여기에 넣으면 로드될때 실행될거임
+    // 요즘은 이렇게 코드 안짬 사람들이 뜨자마자 저런거 허락해달라고 하면 혐오감 생겨서 필요할때만 뜨게 해야함
+  }
+
+
   var a = 1;
   var name = "연락처앱";
   var name2 = ["김영숙", "홍길동", "피자집"];
-  var phone = ["01012341234", "01011111111", "01022222222"];
   var like = [0, 0, 0];
 
   var total =3;
 
-
-  AddPhone(New_phone)  // 다양한 자료형 입력가능
-  {
-    setState(() {
-      phone.add(New_phone); // 자동 스테이트 추가됨 그러면 자동 UI 반영됨 왜냐 스테이스 수정되면 자동 재 랜더링 하기때문
-    });
-  }
 
   AddName(New_name)  // 다양한 자료형 입력가능
   {
@@ -38,24 +55,10 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  Remove_name(r_name)  // 다양한 자료형 입력가능
-  {
-    setState(() {
-      name2.remove(r_name); // 자동 스테이트 추가됨 그러면 자동 UI 반영됨 왜냐 스테이스 수정되면 자동 재 랜더링 하기때문
-    });
-  }
-
-  Sort_name()  // 다양한 자료형 입력가능
-  {
-    setState(() {
-      name2.sort(); // 자동 스테이트 추가됨 그러면 자동 UI 반영됨 왜냐 스테이스 수정되면 자동 재 랜더링 하기때문
-    });
-  }
-
   AddNum()  // 다양한 자료형 입력가능
   {
     setState(() {
-    total++;
+      total++;
     });
   }
 
@@ -70,7 +73,7 @@ class _MyAppState extends State<MyApp> {
               context: context,
               builder: (context) {
 
-                return DialogUI(AddName : AddName, AddNum : AddNum, AddPhone : AddPhone);  // 함수도 이렇게 전송함  변수명 or 함수명만 전송
+                return DialogUI(AddName : AddName, AddNum : AddNum);  // 함수도 이렇게 전송함  변수명 or 함수명만 전송
               });
         },
       ),
@@ -84,42 +87,12 @@ class _MyAppState extends State<MyApp> {
       ),
       bottomNavigationBar: Botoom_Main(),
       body: ListView.builder(
-        itemCount: name2.length,   // 이거때문에 오류 났음 구현은 했는데 값출력인 안된 이유 3만 반복하라고 해서
-        // 이걸 동적으로 바꾸면 됨  name2.length   길이만큼
+        itemCount: name2.length,
         itemBuilder: (context, i) {
           return ListTile(
-            leading: Image.asset('koko1.png'),
-            title: Container(
-              width: 300,
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 80,
-                    child: Text(name2[i]),
-                  ),
-                  Text(phone[i]),
-                  Container(
-                    width: 200,
-                    padding: EdgeInsets.fromLTRB(10, 3, 20, 3),
-                    child:
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(child: Text("삭제"), onPressed: () {
-                        Remove_name(name2[i].toString());
-                      },),
-                      ElevatedButton(child: Text("정렬"), onPressed: () {
-                        Sort_name();
-                      },),
-                    ],
-                  ),
-                  ),
-                ],
-              ),
-            ),
-
+            leading: Image.asset('assets/koko1.png'),  // 앱에서는  이렇게 assets/ 까지 전체 경로 입력 필요함
+            title: Text(name2[i]),
           );
-
         },
       ),
     );
@@ -127,12 +100,11 @@ class _MyAppState extends State<MyApp> {
 }
 
 class DialogUI extends StatelessWidget {
-  DialogUI({Key? key, this.AddName, this.AddNum, this.AddPhone}) : super(key: key);
+  DialogUI({Key? key, this.AddName, this.AddNum}) : super(key: key);
   final AddName;
   final AddNum;
-  final AddPhone;
   var inputData = TextEditingController();
-  var inputData2 = TextEditingController();
+  var inputData2 = '';
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -144,26 +116,17 @@ class DialogUI extends StatelessWidget {
             crossAxisAlignment:CrossAxisAlignment.start,
             children: [
               TextField(
-                controller: inputData,
+                onChanged: (text){ // 사용자가 입력한 값 여기에 저장됨
+                  inputData2 = text;
+                },// onPressd랑 비슷 이건 버튼 누르면 함수 실행해주세요이지만
               ),
-              TextField(
-                controller: inputData2,
-              ),
-  
+
               TextButton(child: Text("완료"),onPressed: () {
-                if(inputData.text == "" || inputData2.text == "")
-                  {
-                    Navigator.pop(context);
-                  }
-                else {
-                  AddName(inputData.text); //.text 를 해서 넘겨 줘야함
-                  AddNum();
-                  AddPhone(inputData2.text); //.text 를 해서 넘겨 줘야함
-                  Navigator.pop(context);
-                }
+                AddName(inputData2);
+                AddNum();
               }, ),
               TextButton(child: Text('취소'),
-                onPressed: (){ Navigator.pop(context);})
+                  onPressed: (){ Navigator.pop(context);})
             ],
           ),
         )
