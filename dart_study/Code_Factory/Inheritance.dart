306d3ec756e2d6866a23127c34f3485fc046da5e
woogie <jeonguk29@naver.com>
2022-07-08 19:22:51 +0900

    Dart 객체지향 프로그래밍  상속 (Inheritance)2, Type Comperison
    정리 : 상속을 하면 부모클레스와 자식클래스가 생기고 부모클레스의 속성들이 자식클레스로 다 넘어간다
    자식 클레스 속성들은 부모클레스 넘어가지 않는다 그리고 같은 자식 클레스 끼리도 속성공유를 하지 않는다.
    OOP 핵심개념임

--- dart_study/Code_Factory/Inheritance.dart
+++ dart_study/Code_Factory/Inheritance.dart
@@ -5,17 +5,52 @@ void main()
 
   apink.sayname();
   apink.sayMembersCount();
+  //apink.sayMale();  불가능 부모에서 자식거는 사용 못함 
 
 
-  print('--------BTs---------');
-  BoyGroup bts =BoyGroup("bts", 7); // 이렇게 넘겨줘도   BoyGroup 에는 아무 값도 없지만  Idol 클래스를 상속받아 모든걸 사용가능함
+  print('--------Boy Grouo---------');
+  BoyGroup bts =BoyGroup("bts", 7); 
   bts.sayMembersCount();
-  bts.sayname();  // 이렇게 하면 사실상 부모에 있는 걸 접근해서 그대로 쓰는 거임 
-}
+  bts.sayname();  
+  bts.sayMale();
+  //bts..sayFemale();  사용못함 걸그룹에만 있고 그냥 공통점은 Idol 클래스를 상속 받는거 뿐 관계가 없음 
 
-// 상속 inheritance
-// 상속을 받으면 부모 클래스의 모든 속성을 
-// 자식 클래스가 부여 받는다. 
+  print('--------Girl Group---------');
+  GirlGroup redvelvet = GirlGroup("Red velvet", 5); 
+  redvelvet.sayMembersCount();
+  redvelvet.sayname();  
+  redvelvet.sayFemale();
+
+
+
+print('---------Type Comperison------------');
+print(apink is Idol);
+print(apink is BoyGroup);
+print(apink is GirlGroup);
+
+
+print('---------Type Comperison------------');
+print(bts is Idol);
+print(bts is BoyGroup);   // 상속시 자식클레스는 자식 클레스 뿐만아니하 부모 클레스도 될수 있음 
+print(bts is GirlGroup);
+
+print('---------Type Comperison------------');
+print(redvelvet is Idol);
+print(redvelvet is BoyGroup);   // 상속시 자식클레스는 자식 클레스 뿐만아니하 부모 클레스도 될수 있음 
+print(redvelvet is GirlGroup);
+
+
+// 부모 클레슨 자식 클레스가 되지 못하지만 부모 클레스의 인스턴스인 bts ,redvelvet 부모클레스, 자식 클레스 (자기자신)됨 
+}
+/*
+
+정리 : 상속을 하면 부모클레스와 자식클래스가 생기고 부모클레스의 속성들이 자식클레스로 다 넘어간다 
+자식 클레스 속성들은 부모클레스 넘어가지 않는다 그리고 같은 자식 클레스 끼리도 속성공유를 하지 않는다.
+
+*/
+
+
+
 
 
 class Idol{
@@ -49,4 +84,24 @@ class BoyGroup extends Idol{  // extends 부모클레스  : 상속 받겠다
             membersCount : membersCount,    // 그러면 부모클래스는 자동으로 생김  그럼 부모는 자동으로 BTS 7 값을 가지고 생성됨 
           
           );
+  
+  void sayMale()
+  {
+    print("저희는 남자 아이돌입니다.");
+  }
+}
+
+class GirlGroup extends Idol{  // extends 부모클레스  : 상속 받겠다 
+  GirlGroup(// 생성자에서 아래와 같은 맴버 변수를 받고 
+    String name,    
+    int membersCount,  
+  ) : super(name : name,  // super 키워드로 Idol 클래스에 생성자로 전송함     그냥 그대로 받은거 부모로 전달임 
+            membersCount : membersCount,    // 그러면 부모클래스는 자동으로 생김  그럼 부모는 자동으로 BTS 7 값을 가지고 생성됨 
+          
+          );
+  
+  void sayFemale()
+  {
+    print("저희는 여자 아이돌입니다.");
+  }
 }
