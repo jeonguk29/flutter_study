4aaf9351d9afea6cb1ead0f478bfb7ffe18f302c
woogie <jeonguk29@naver.com>
2022-07-11 17:01:37 +0900

    다른 파일로 빼서 import하는 법 ThemeData안에서 버튼스타일 주는 법 레이아웃 중간에 ThemeData 생성하는법

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+//import './style.dart';  // main.dart랑 같은 경로에 있어서 그냥 이렇게 해주면됨
+import './style.dart' as style; // import 할때 변수 중복문제 해결 maindart에도 theme 변수 있을수 있으니
+
+
 
 void main() {
   runApp(
-      MaterialApp(
-        theme: ThemeData(
-          //iconTheme: IconThemeData(color: Colors.grey),
-          appBarTheme: AppBarTheme(
-              color: Colors.white,
-              actionsIconTheme: IconThemeData(color: Colors.black, size: 35.0),
-              titleTextStyle: TextStyle(color: Colors.black, fontSize: 20.0),
-          ),
-
-          textTheme: TextTheme(
-            bodyText2: TextStyle(color: Colors.red)
-          ),
-        ),// 스타일 모아 놓는 ThemeData()   css 파일 같은 것임
+      MaterialApp(    // 스타일이 길어지면 외부 파일로 만들어서 가져올수 있음
+                      // 즉 긴코드를 다른 파일로 빼는법
+        // theme, 다른 파일 불러와서 다른 파일 변수 사용 가능함
+        theme: style.theme, // 이렇게 사용 가능함
         home: MyApp()
     )
   );
@@ -40,10 +35,25 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text("Instagram"),
         actions: [
-          Icon(Icons.add_box_outlined)
+          IconButton(
+            icon: Icon(Icons.add_box_outlined),
+            onPressed: (){},
+            iconSize: 30,
+          )
         ],
       ),
+      body: Theme(
+        data: ThemeData(
 
+        ),
+        child: Container(  // 레이아웃 중간에 ThemeData() 생성가능 ex 얘의 자식부터는 모든 글자색이 달라졌으면 좋겠다
+          // Theme으로 감싸고 data 안에 ThemeData() 안에 스타일주면
+          // child 속성 위젯 이안에 자식들은 다 이 스타일을 따라감
+          // 위에 style.theme 이 아니라 원칙1 : 나랑 조금더 가까운 스타일 먼저 적용 하려고함
+
+        ),
+      ),
+      //TextButton(onPressed: (){},child: Text('안녕'),),
     );
 
   }
---
+++ instagram/lib/style.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart'; // 이것도 넣어줘야 아래 위젯들 작동됨
+
+// ThemeData 부분만 변수에 담음
+
+var _var1;
+/*
+언더바 붙이면 자동으로 "다른 파일에서 import해서 쓸 수 없는 변수"가 됩니다.   : Private 속성
+함수명, 클래스명에도 통하니까 다른 파일에서 쓰기 싫은 변수들은 언더바를 쓰도록 합시다.
+*/
+
+
+var theme = ThemeData(
+  textButtonTheme: TextButtonThemeData(  // 여기만 이름 바꾸면 다른 버튼도 동일함
+    style: TextButton.styleFrom( // 버튼 스타일 변경시 여기 안에서 바꿔야 모든 버튼 적용됨
+      backgroundColor: Colors.grey,
+    )
+  ),
+
+//iconTheme: IconThemeData(color: Colors.grey),
+appBarTheme: AppBarTheme(   // 이렇게 길어졌을때 다른 파일에 넣어놓고 불러오기 가능
+color: Colors.white,
+elevation: 1, // 그림자
+actionsIconTheme: IconThemeData(color: Colors.black, size: 35.0),
+titleTextStyle: TextStyle(color: Colors.black, fontSize: 25.0),
+),
+
+textTheme: TextTheme(
+bodyText2: TextStyle(color: Colors.red)
+),
+);
