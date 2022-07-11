fd828bff7dde8d18d4461c31f077ba4869403d51
woogie <jeonguk29@naver.com>
2022-07-11 16:14:56 +0900

    ThemeData 사용시 특징1 : 위젯은 나랑 가까운 스타일을 가장 먼저 적용 ThemeData 사용시 특징 2 복잡한 위젯은 복잡한 위젯 theme()안에서 스타일 줘야할 수도있음 Text()의 스타일을 변경하고 싶으면 : textTheme로 전체 바꾸기 + 변수로 빼서 사용(부분)

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,115 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      MaterialApp(
+        theme: ThemeData(
+          iconTheme: IconThemeData(color: Colors.blue), // 이렇게 하면 아이콘 전부 블루색상 지정됨
+          // 장점 : 이제 모든 아이콘은 파래짐 (인스타 그램 보면 색상 통일성 있음)
+            //하지만 여기서 아이콘 스타일 줘 밨자 AppBar 안은 (아이콘)불가능
+          appBarTheme: AppBarTheme(
+              color: Colors.grey,
+              actionsIconTheme: IconThemeData(color: Colors.blue)// ThemeData 사용시 특징 2 복잡한 위젯은 복잡한 위젯 theme()안에서 스타일 줘야할 수도
+            // 그래서 아이콘 전체 위에 해줬지만 위젯은 나랑 가까운 스타일을 가장 먼저 적용하기 때문에 이렇게 한번 더 준거임
+            // AppBar 안에 엑션 안에도 디폴트로 주라고
+          ),
+
+          textTheme: TextTheme(
+            bodyText2: TextStyle(color: Colors.red)
+                /*
+                bodyText2 이런걸 하나 결정해줘야하는데
+                글자 위젯들은 이러한 당양한 스타일중 하나를 가져다 씀
+                ex
+                Text 위젯은 이 중에 bodyText2 스타일을 사용하고
+                AppBar와 Dialog 위젯은 이 중에 headline6을 사용하고
+                ListTile 위젯은 이 중에 subtitle1을 사용하고
+                그런 식으로 정해져있습니다.  다 외우고 알필요 x
+
+                 */
+          ),
+        ),// 스타일 모아 놓는 ThemeData()   css 파일 같은 것임
+        home: MyApp()
+    )
+  );
 }
+
+
+var a = TextStyle(
+
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    /*
+    return MaterialApp(       // MaterialApp 밖으로 빼야 나중에 편해서 빼줄거임
+      home: Scaffold(),
     );
-  }
-}
+    */
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        actions: [
+          Icon(Icons.star) // 위에서 전체 블루지정했는데 이놈은 하얀색임 ThemeData 사용시 특징1 : 위젯은 나랑 가까운 스타일을 가장 먼저 적용
+        ],
+      ), // 스타일 잘 넣는법  Text('sdf', style: ,) 이런식으로 스타일 넣으면 길어짐 따로 분리 할수 있음
+      body: Column(
+        children: [
+          Icon(Icons.star),
+          Text('안녕'),
+          Text('안녕', style: a), // 하지만 TextTheme 사용하기 복잡하다 싶으면 변수로 빼서 사용할수도 있음 특히 글자에 경우 이렇게 사용하는게 더 편할거임
+          // 전체 글자는 TextTheme 사용하고 세부 조정 변수로 할수도 있음
+        ],
+      )
+       //Icon(Icons.star), // Icon(Icons.star, color: Colors.blue),
+
     );
+
   }
 }
