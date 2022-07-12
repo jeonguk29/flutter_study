5713d04bc6ee3f433a43788d55c233334cef3e8f
woogie <jeonguk29@naver.com>
2022-07-12 21:05:07 +0900

    서버에서 게시물 데이터 3개 가져와서 게시물 위젯안에 뿌리기 + 위젯과 위젯 사이에 값 전달 및 서버 연결 실패시 예외처리

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -15,10 +15,6 @@ void main() {
 }
 
 
-var a = TextStyle(
-
-);
-
 
 
 
@@ -33,16 +29,19 @@ class _MyAppState extends State<MyApp> {
   var tab = 0;
   var list = [1,2,3]; // 리스트는 이렇게 만듬
   var map = {'name':'john', 'age':20};
+  var mamber = [];
   // map 은 키 와 벨류로 구분 해서 만듬
 // 참고로 list와 map안에는 서로 list , map 넣어도 상관 없음 아무거나 다 넣을수 있음
 
   getData()async{
     // get요청  보낼때 이렇게
     var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json')); // 결과 반환 해주면 변수에 담아 쓰면됨
-    // http 함수도 오래걸리는 함수임 (전문용어로 Future) 그래서 await 붙어줌 await 쓰려면 함수 뒤에 async 키워드 필요
-    //print(result.body);  다 "" 로 되어있음  서버랑 주고받는 데이터는 오직 문자만 가능
-    var result2 = jsonDecode(result.body);
-    print(result2[0]['likes']);
+    if (result.statusCode == 200) {  // 에러 체크
+      this.mamber  = jsonDecode(result.body);
+    } else {
+      throw Exception('실패함ㅅㄱ');
+    }
+    //print(result2[0]['likes']);
     //조작하기 쉬운 list, map같은 자료로 변환하려면 json.Decode() 여기에 넣었다가 빼면 됩니다.
     // 현제 [{}] 이구조임  이걸 보니 리스트자료형 안에 map 자료형이 있는거임
   }
@@ -55,7 +54,6 @@ class _MyAppState extends State<MyApp> {
   }
 
 
-  var name2 = ["김영숙", "홍길동", "피자집"];
 
   @override
   Widget build(BuildContext context) {
@@ -73,7 +71,7 @@ class _MyAppState extends State<MyApp> {
         ],
 
       ),
-      body: [Home(), Text('샵페이지')][tab],  // 0과 1에 따라 보이는게 다름
+      body: [Home(mamber:mamber), Text('샵페이지')][tab],  // 0과 1에 따라 보이는게 다름
       bottomNavigationBar: BottomNavigationBar(
       showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -102,10 +100,11 @@ class _MyAppState extends State<MyApp> {
 }
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
+  const Home({Key? key, this.mamber}) : super(key: key);
+  final mamber;
   @override
   Widget build(BuildContext context) {
+
     return ListView.builder(
       itemCount: 3,
       itemBuilder: (context, i) {
@@ -113,11 +112,18 @@ class Home extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // 웹상 이미지 넣을수 있음
-              Image.network('https://mblogthumb-phinf.pstatic.net/MjAxNzAxMjZfMjEw/MDAxNDg1NDIxNjIxOTA2.yCkDe0TNtSfretPURTVrdDqay50odvbiNIyDGIG8bEgg._mge-2IK2a4RsLDkU-etvMmgNn-R66yPCFFKIKkut3Yg.JPEG.momossense/Deja_Vu_%28feat._Justin_Bieber%29_-_Single_2.jpg?type=w800'),
-              Text("좋아요"),
-              Text("글쓴이"),
-              Text('글내용'),
+              /*
+
+              [{id: 0, image: https://codingapple1.github.io/app/img0.jpg, likes: 5, date: July 25, content: 등을 대라 등대, liked: false, user: John Kim}, {id: 1, image: https://codingapple1.github.io/app/img1.jpg, likes: 22, date: Aug 3, content: 요즘 폰카는 성능이 좋습니다, liked: false, user: Hilton_m}, {id: 2, image: https://codingapple1.github.io/app/img2.jpg, likes: 10, date: Nov 30, content: 기린바보 😵, liked: false, user: rimvely1}]
+
+               */
+              Image.network(mamber[i]['image']),
+              Text(mamber[i]['id'].toString()),
+              Text(mamber[i]['likes'].toString()),
+              Text(mamber[i]['date'].toString()),
+              Text(mamber[i]['content'].toString()),
+              Text(mamber[i]['liked'].toString()),
+              Text(mamber[i]['user'].toString()),
             ],
           ),
         );
