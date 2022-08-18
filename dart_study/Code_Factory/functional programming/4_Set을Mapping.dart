e97ac0ae4c46c9a2cfd70c188a5f92bdf794aa5a
woogie <jeonguk29@naver.com>
2022-08-18 17:19:33 +0900

    Set 자료형을 mapping 하는 방법 리스트와 다를게 없음

---
+++ dart_study/Code_Factory/functional programming/4_Set을Mapping.dart
@@ -0,0 +1,69 @@
+// Set 자료형을 mapping 하는 것은 리스트와 다를게 없음똑같음 
+
+void main(){
+ Set blackPinkSet = {
+   '로제',
+   '지수',
+   '제니',
+   '리사',
+ }; 
+  
+ final newSet = blackPinkSet.map((x) => "블랙핑크 $x").toSet();
+ print(newSet);
+ 
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
