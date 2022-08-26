8812d918bb92774761f6cb081b867fb50b59235c
woogie <jeonguk29@naver.com>
2022-08-26 21:58:39 +0900

    SignInScreen 위젯을 통해 파이어베이스 로그인을 위한 기본 셋팅 완료 (피키지 설치 등) https://www.youtube.com/watch?v=J3tETIWLDW8&t=5s 참조

---
+++ login/signInscreen_login_firebase/android/app/src/main/kotlin/com/example/apps_to_save_money_while_quitting_smoking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apps_to_save_money_while_quitting_smoking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ login/signInscreen_login_firebase/android/app/src/main/kotlin/com/example/signlnscreen_widget_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.signlnscreen_widget_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
