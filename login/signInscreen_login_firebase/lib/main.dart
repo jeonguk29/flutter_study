e5f3b3cff867a2ab5cd1dc86d22d2bfbe9596f43
woogie <jeonguk29@naver.com>
2022-08-29 22:24:14 +0900

    SignInScreen 위젯을 통해 파이어베이스 로그인인 구현 실제 파이어베이스에 적용됨

--- login/signInscreen_login_firebase/lib/main.dart
+++ login/signInscreen_login_firebase/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutterfire_ui/auth.dart';
 
-void main() {
-  runApp( MyApp());
+import 'chat_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +16,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Authentication(),
+    );
+  }
+}
+
+class Authentication extends StatelessWidget {
+  const Authentication({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            headerBuilder: (context, constraints, double) {
+              return Padding(
+                padding: EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image(
+                    image: AssetImage('image/login_main.png'),
+                  ),
+                ),
+              );
+            },
+            providerConfigs: [EmailProviderConfiguration()],
+          );
+        }
+        return ChatScreen();
+      },
+    );
   }
 }
